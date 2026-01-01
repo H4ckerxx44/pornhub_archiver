8bc47ca6ea730b746e5f9ef8a6f95e8cc47468de
H4ckerxx44 <dennis.wortmann1999@gmail.com>
2026-01-01 04:25:21 +0100

    weird error .so files missing

--- run.py
+++ run.py
@@ -12,8 +12,8 @@ SLEEP_INTERVAL = int(os.getenv("SLEEP_INTERVAL", 3600))
 
 
 MAJOR = 1
-MINOR = 3
-PATCH = 1
+MINOR = 4
+PATCH = 0
 
 # ROOT_PATH = pathlib.Path("F:/auto_dl")
 # SLEEP_INTERVAL = 3600
