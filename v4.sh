430dc82934f8a5be7b7ba3d7a2b1a06434546a6f
H4ckerxx44 <dennis.wortmann1999@gmail.com>
2026-06-12 22:41:47 +0200

    chore: add latest branch to v4 docker script

--- v4.sh
+++ v4.sh
@@ -1,1 +1,2 @@
 docker build -t h4ckerxx44/pornhub_archiver:v4 . && docker push h4ckerxx44/pornhub_archiver:v4
+docker build -t h4ckerxx44/pornhub_archiver:latest . && docker push h4ckerxx44/pornhub_archiver:latest
