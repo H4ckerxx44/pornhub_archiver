f888bbe5d65caaa32f977c48e0c2e6e8a05928bc
H4ckerxx44 <dennis.wortmann1999@gmail.com>
2026-06-12 22:56:43 +0200

    chore: bump version

--- src/run.py
+++ src/run.py
@@ -14,7 +14,7 @@ ROOT_PATH = pathlib.Path(os.getenv("ROOT_PATH", "/data"))
 SLEEP_INTERVAL = int(os.getenv("SLEEP_INTERVAL", 3600))
 OPENSSL_CONF = os.getenv("OPENSSL_CONF")
 
-VERSION = (4, 1, 0)
+VERSION = (4, 1, 1)
 
 
 async def main() -> None:
